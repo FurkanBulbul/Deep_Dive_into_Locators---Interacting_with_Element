e147268c5ea4b5c51d24a457e163bdc8fa3db94b
Furkan Bülbül <74109076+FurkanBulbul@users.noreply.github.com>
2022-12-29 12:42:00 +0300

    Update CheckBoxes.java

--- src/Interacting_with_Element/CheckBoxes.java
+++ src/Interacting_with_Element/CheckBoxes.java
@@ -7,13 +7,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class CheckBoxes {
 
     public static void main(String[] args) {
+
         System.setProperty("webdriver.chrome.driver",
                 "drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-
         driver.get("https://automationtesting.co.uk/dropdown.html");
-
         System.out.println(driver.findElement(
                 By.cssSelector("input#cb_red")).isSelected()
         );
