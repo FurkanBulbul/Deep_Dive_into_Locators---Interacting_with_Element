5f6e086e9bc8f32333e94aee9299adf4513dec05
Furkan Bülbül <74109076+FurkanBulbul@users.noreply.github.com>
2022-12-31 17:33:59 +0300

    Update ClickingButtons.java

--- src/Interacting_with_Element/ClickingButtons.java
+++ src/Interacting_with_Element/ClickingButtons.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ClickingButtons {
     public static void main(String[] args) {
+        
         // system set property added
         System.setProperty("webdriver.chrome.driver",
                 "drivers/chromedriver.exe");
